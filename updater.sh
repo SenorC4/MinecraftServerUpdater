668ff6bdf6126f3f6edc0287209068f08b6bad3f
senorc4 <lukelecain@gmail.com>
2025-06-26 13:32:21 +0000

    updated the script to pull the version and download url from the json databse instead of scraping the download site because they changed how the site is rendered

--- updater.sh
+++ updater.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Fetching the HTML page with curl and extracting the relevant line with grep
-durl=$(curl -s --user-agent "Mozilla/5.0 (platform; rv:gecko-version) Gecko/gecko-trail Firefox/firefox-version" https://www.minecraft.net/en-us/download/server | grep server.jar)
+durl=$(curl -s $(curl -s https://piston-meta.mojang.com/mc/game/version_manifest.json | jq -r '.latest.release as $latest | .versions[] | select(.id == $latest) | .url') | jq -r '.downloads.server.url')
 
 # Extracting the Minecraft version from the line
-minever=$(echo "$durl" | awk -F'[<>]' '{print $3}')
+minever=$(curl -s https://piston-meta.mojang.com/mc/game/version_manifest.json | jq -r '"minecraft_server.\(.latest.release).jar"')
 
 # Extracting the URL from the line
-mineurl=$(echo "$durl" | awk -F'"' '{print $2}')
+#mineurl=$(echo "$durl" | awk -F'"' '{print $2}')
 
 # Check if a local version exists
 local_file=$(ls minecraft_server*.jar 2>/dev/null)
@@ -26,17 +26,17 @@ if [ -n "$local_file" ]; then
         echo "Local version ($local_file) is up to date."
     else
         echo "Updating to new version ($minever)..."
-        rm *.jar
-        wget "$mineurl" -O "$minever"
-        cp "$minever" server.jar
-        updated=true
+	rm *.jar
+        wget "$durl" -O "$minever"
+	cp "$minever" server.jar
+	updated=true
     fi
 else
     # Download if no local version exists
     echo "No local version found. Downloading $minever..."
     rm *.jar
-    wget "$mineurl" -O "server.jar"
-    cp server.jar "$minever"
+    wget "$durl" -O "$minever"
+    cp "$minever" server.jar
     #kill and rerun minecraft
     updated=true
 fi
@@ -44,12 +44,12 @@ fi
 # Check if the Minecraft tmux session is running
 if tmux has-session -t minecraft 2>/dev/null; then
     echo "Minecraft server is running in a tmux session."
-
+    
     # If an update occurred, kill the tmux session and restart
     if [ "$updated" = true ]; then
         echo "Stopping Minecraft server tmux session to apply the update..."
         tmux kill-session -t minecraft
-
+        
         # Start the updated Minecraft server in a new tmux session
         start_minecraft
     else
@@ -59,7 +59,7 @@ else
     # Start the server in a new tmux session if it's not running and an update was applied
     if [ "$updated" = true ]; then
         echo "Minecraft server is not running, update was needed. Starting..."
-        start_minecraft
+	start_minecraft
     else
         echo "Minecraft server is not running, no update was needed. Starting..."
         start_minecraft
